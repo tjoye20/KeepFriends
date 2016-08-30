d2b54fc9c84653e7d3d1661f8d871e17875633b1
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-30 13:12:24 -0500

    Test Tom's number.

--- text_friends.rb
+++ text_friends.rb
@@ -5,13 +5,13 @@ Dotenv.load
 array_of_friends = [ENV['ray'], ENV['jen'], ENV['akeem'], ENV['gabe'], ENV['samuel'], ENV['brandon']]
 my_num = ENV['my_number']
 
-text_message = ["Hey, how're things on your end?", "Hey, wassup?"]
+text_message = ["Hey, it's Tj. How're things on your end?", "Hey, it's Tj. Wassup?"]
 
 
 @client = Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']
 
 @client.messages.create(
   from: my_num,
-  to: array_of_friends.sample,
+  to: "5126981651",
   body: text_message.sample
 )
