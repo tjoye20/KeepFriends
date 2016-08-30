6d0b3b93cba0d838a458d7650198038e3088d69c
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-30 14:56:27 -0500

    Add cron job. Update readme.

--- text_friends.rb
+++ text_friends.rb
@@ -1,16 +1,17 @@
+require 'twilio-ruby'
 require 'dotenv'
 Dotenv.load
 
 array_of_friends = [ENV['ray'], ENV['jen'], ENV['akeem'], ENV['gabe'], ENV['samuel'], ENV['brandon']]
 my_num = ENV['my_number']
 
-text_message = ["Hey, it's Tj. How're things on your end? Text back my regular number to confirm you got this.", "Hey, it's Tj. Wassup? Text back my regular number to confirm you got this."]
+text_message = ["Hey, how're things on your end?", "Hey, wassup?"]
 
 
 @client = Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']
 
 @client.messages.create(
   from: my_num,
-  to: "4692746225",
+  to: array_of_friends.sample,
   body: text_message.sample
 )
